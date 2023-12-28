fc8717f36ac51ee6777e01170276cc934b7bdb5e
andmholm <andmholm@iu.edu>
2023-12-28 17:52:09 -0500

    fixed header guard and made header foe singly:

--- linkedlist/singly.hpp
+++ linkedlist/singly.hpp
@@ -1,6 +1,26 @@
 #ifndef SINGLY_LIST_HPP
-#define SINGLE_LIST_HPP
+#define SINGLY_LIST_HPP
+#include "./node.hpp"
 
+template <typename T>
 
+struct SinglyList {
+
+protected:
+    int nelem;
+    Node<T>* head;
+    Node<T>* tail;
+
+public:
+    SinglyList();
+    void add(T item);
+    // void insert(int index, T item);
+    // void remove(T item);
+    // void overwrite(T item, T item);
+    // bool contains(T item);
+    // void print();
+    int size();
+    bool empty();
+};
 
 #endif
