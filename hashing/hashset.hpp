cb591277e4547fc48e73000947ecda8006aa4728
Andrew <andrewholmes011002@gmail.com>
2024-01-09 14:12:32 -0500

    skeleton

--- hashing/hashset.hpp
+++ hashing/hashset.hpp
@@ -1,13 +1,25 @@
 #ifndef HASHSET_HPP
 #define HASHSET_HPP 
 
-#include "../link/node.hpp"
-#include "./doubly.hpp"
+#include "vector"
+#include "array"
+#include "../utils/node.hpp"
+#include "../linkedlist/doubly.hpp"
 
+template <typename T>
 class HashSet {
 
+    std::vector<Node<T>> elements;
+    int n_elements;
 
-
+public:
+    HashSet();
+    HashSet(const T arr[], int size);
+    HashSet(const std::array arr);
+    HashSet(const std::vector vec);
+    hash(T item);
+    rehash();
+    add(T item);
 };
 
 #endif
