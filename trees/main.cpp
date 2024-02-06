0d33669274ef7ef6ae86895154fc08f7139d6b6b
Andrew Holmes <andrewholmes011002@gmail.com>
2024-02-06 16:50:20 -0500

    bst complete

--- trees/main.cpp
+++ trees/main.cpp
@@ -7,6 +7,8 @@ int main() { BST<std::string, int> tree;
     tree.add("Mike", 21);
     tree.add("Bill", 19);
     tree.add("Sarah", 18);
+    printf("Size: %d\n", tree.size());
+    tree.add("Baker");
     tree.print();
     tree.remove("Sarah");
     tree.print();
@@ -15,4 +17,6 @@ int main() { BST<std::string, int> tree;
     printf("%s -> %d\n", "John", tree.get("John").value_or(-1));
     printf("Size: %d\n", tree.size());
     printf("%s -> %d\n", "Rufus", tree.get("Rufus").value_or(-1));
+    printf("Contains %s? %d\n", "Mike", tree.contains("Mike"));
+    printf("Contains %s? %d\n", "Sarah", tree.contains("Sarah"));
 }
